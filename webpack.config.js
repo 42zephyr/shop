02971d9db490350eff69869fe94ed30807add4ae
Petunin Alexandr <zephyr.42.foton@gmail.con>
2020-10-22 21:27:11 +0300

    first ch day 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,36 @@
 var path = require('path');
+const VueLoaderPlugin=require('vue-loader/lib/plugin')
 module.exports = {
-  //...
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+
   resolve: {
     alias: {
       vue: 'vue/dist/vue.js'
     }
   },
+
+  module:{
+    rules:[
+      {
+      test: /\.vue$/,
+      loader:'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader:'babel-loader'
+        },
+      {
+        test: /\.css$/,
+        loader:'vue-style-loader'
+        },
+    ]
+  },
+
+  plugins:[
+    new VueLoaderPlugin()
+  ],
+
   devServer: {
     contentBase: path.join(__dirname, 'src'),
     compress: true,
